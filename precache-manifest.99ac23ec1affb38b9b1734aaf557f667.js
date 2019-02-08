29a4ab819cbe49d0a07b0b3e3f923e5b028bec39
Gian Lucca Ricupito Dos Santos <gian.santos@saraiva.com.br>
2019-02-08 11:57:19 -0200

    Updates

---
+++ precache-manifest.99ac23ec1affb38b9b1734aaf557f667.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/crud_react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "95a1b8153e4676e8495b",
+    "url": "/crud_react/static/css/main.859731ad.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/crud_react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2419411f9b04d24957bc",
+    "url": "/crud_react/static/js/1.2419411f.chunk.js"
+  },
+  {
+    "revision": "abff66de8384bf8b201e",
+    "url": "/crud_react/static/js/runtime~main.abff66de.js"
+  },
+  {
+    "revision": "b5abf8a56c73268631f5aade7b5ea761",
+    "url": "/crud_react/static/media/logo.b5abf8a5.png"
+  },
+  {
+    "revision": "95a1b8153e4676e8495b",
+    "url": "/crud_react/static/js/main.95a1b815.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/crud_react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/crud_react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/crud_react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "2419411f9b04d24957bc",
+    "url": "/crud_react/static/css/1.53821538.chunk.css"
+  },
+  {
+    "revision": "f75630aea1db324cb387d4fa9cd5809e",
+    "url": "/crud_react/index.html"
+  }
+];
